f10f8fa062204dcf98cc840b11ce8ebd46469b62
MINJOO-KIM <gkminju2000@gmail.com>
2024-05-17 14:48:18 +0900

    chore: vue 초기 설정

---
+++ frontend/movie-recommend/src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { RouterLink, RouterView } from "vue-router";
+import HelloWorld from "./components/HelloWorld.vue";
+</script>
+
+<template>
+  <div>
+    <RouterLink to="/">Home</RouterLink>
+    <RouterLink to="/about">About</RouterLink>
+  </div>
+
+  <RouterView />
+</template>
+
+<style scoped></style>
