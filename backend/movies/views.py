c5cbac781abc05897260ef13e8bb5e695412d7bf
Kim Seonmin <w.hand715@gmail.com>
2024-05-19 01:10:42 +0900

    feat : 유저 선호 기반 영화 추천 기능 개발

--- backend/movies/views.py
+++ backend/movies/views.py
@@ -4,8 +4,8 @@ from rest_framework.response import Response
 from rest_framework import status
 
 from .models import Genre, Movie
-
 from .serializers import GenreSerializer, MovieListSerializer
+from .functions import *
 
 import random
 # Create your views here.
@@ -15,40 +15,27 @@ def all_genres(request):
     serializer = GenreSerializer(instance=genres, many=True)
     return Response(serializer.data, status=status.HTTP_200_OK)
     
-@api_view(['GET'])
-def movies_recommend(request):
-    # best_movie = request.GET.get('best-movie')
-    # recommended = request.GET.get('recommended').split(',')
-    # genres = request.GET.get('genres', '-1').split(',')
-    # directors = request.GET.get('directors', '-1').split(',')
-    # actors = request.GET.get('actors', '-1').split(',')
 
     
-    # recommended = -1 : 영화를 추천받은 내역이 없다
-    # genres = -1 : 선호하는 장르가 없다
-    # directors = -1 : 선호하는 감독이 없다
-    # actors = -1 : 선호하는 배우가 없다
-
-    # print(best_movie)
-    # print(recommended)
-    # print(genres)
-    # print(directors)
-    # print(actors)
-
-
-    # random 추천 기능
-    last_id = Movie.objects.last().id
-    movie_id_list = random.sample(range(1, last_id), 5)
-    movies = Movie.objects.filter(pk__in=movie_id_list)
-    serializer = MovieListSerializer(instance=movies, many=True)
-    return Response(serializer.data, status=status.HTTP_200_OK)
+@api_view(['GET'])
+def movies_recommend(request):
+    movie_title_input = request.GET.get('best-movie', None)
+    # 필수 파라미터 미입력 시 400
+    if movie_title_input == None:
+        return Response({"message":"best-movie is required"},
+                        status=status.HTTP_400_BAD_REQUEST)
+    
+    # 선택 파라미터 및 필수 파라미터 활용해 필터링 정보 만들기
+    filtering_infos = make_filtering_infos(request, movie_title_input)
+    print(filtering_infos)
+    response_data = recommend_movies(filtering_infos)
+    return Response(response_data, status=status.HTTP_200_OK)
 
 
 @api_view(['GET'])
 def movie_detail(request, movie_id):
     try:
         movie = Movie.objects.get(id=movie_id)
-
         response_data = {
             'movieId': movie.id,
             'posterUrl': movie.poster_url,
@@ -60,7 +47,7 @@ def movie_detail(request, movie_id):
             'overview': movie.overview,
             'platforms': [platform.get('name') for platform in movie.platforms.all().values()]
         }
-
         return Response(response_data, status=status.HTTP_200_OK)
+    
     except Movie.DoesNotExist:
         return Response({"message": "Invalid Movie ID"}, status=status.HTTP_404_NOT_FOUND)
