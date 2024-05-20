22e955f17e6a583f877dfaa4876cc812c7e85063
MINJOO-KIM <gkminju2000@gmail.com>
2024-05-20 21:15:48 +0900

    feat: 취향 입력 및 선택 기능 구현

--- frontend/movie-recommend/src/components/movies/Recommend.vue
+++ frontend/movie-recommend/src/components/movies/Recommend.vue
@@ -3,6 +3,33 @@
     <div class="title">당신의 취향을 알려주세요</div>
     <div class="recommend-input">
       <form @submit.prevent="submitForm">
+        <div class="gender">
+          <div>성별</div>
+          <button
+            type="button"
+            :class="['gender-btn', { selected: gender === 'male' }]"
+            @click="selectGender('male')"
+          >
+            남
+          </button>
+          <button
+            type="button"
+            :class="['gender-btn', { selected: gender === 'female' }]"
+            @click="selectGender('female')"
+          >
+            여
+          </button>
+        </div>
+        <div class="age">
+          <label for="age">나이</label>
+          <div class="age-input-container">
+            <input type="number" id="age" name="age" v-model="age" min="0" />
+          </div>
+        </div>
+        <div class="best-movie">
+          <label for="best-movie">최근에 재미있게 본 영화</label>
+          <input type="text" id="best-movie" v-model="bestMovie" />
+        </div>
         <div class="genres">
           <div>선호하는 장르</div>
           <div v-for="genre in genres" :key="genre.id">
@@ -18,6 +45,15 @@
             </button>
           </div>
         </div>
+        <div class="directors">
+          <label for="directors">선호하는 감독</label>
+          <input type="text" id="directors" v-model="directors" />
+        </div>
+        <div class="actors">
+          <label for="actors">선호하는 배우</label>
+          <input type="text" id="actors" v-model="actors" />
+        </div>
+        <br /><br />
         <input type="submit" value="제출" />
       </form>
     </div>
@@ -39,6 +75,11 @@ const selectedGenres = ref([]);
 const directors = ref("");
 const actors = ref("");
 
+// 성별 선택 함수
+const selectGender = (selectedGender) => {
+  gender.value = selectedGender;
+};
+
 // 장르 선택 토글 함수
 const toggleGenre = (genreId) => {
   if (selectedGenres.value.includes(genreId)) {
@@ -46,6 +87,36 @@ const toggleGenre = (genreId) => {
   } else {
     selectedGenres.value.push(genreId);
   }
+};
+
+// 폼 제출 핸들러 함수
+const submitForm = () => {
+  const formData = {
+    bestMovie: bestMovie.value,
+    selectedGenres: selectedGenres.value,
+    directors: directors.value,
+    actors: actors.value,
+  };
+  console.log("Form Data: ", formData);
+
+  const API_URL = "http://127.0.0.1:8000";
+
+  axios({
+    method: "GET",
+    url: `${API_URL}/movies/recommend`,
+    params: {
+      "best-movie": bestMovie.value,
+      genres: selectedGenres.value,
+      directors: directors.value,
+      actors: actors.value,
+    },
+  })
+    .then((res) => {
+      console.log(res);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 };
 
 onMounted(() => {
@@ -57,7 +128,8 @@ onMounted(() => {
 input {
   color: black;
 }
-.genre-btn {
+.genre-btn,
+.gender-btn {
   background-color: black;
   color: white;
   width: 100px;
@@ -65,12 +137,14 @@ input {
   border-radius: 2rem;
   margin: 5px;
 }
-.genre-btn:hover {
+.genre-btn:hover,
+.gender-btn:hover {
   transition: background-color 0.3s;
   background-color: #424242;
   opacity: 85%;
 }
-.genre-btn.selected {
+.genre-btn.selected,
+.gender-btn.selected {
   background-color: #424242;
   opacity: 85%;
 }
