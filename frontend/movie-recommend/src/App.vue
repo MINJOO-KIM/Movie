bc2c91d4a1d7660191cd8a09b373064d44c7d0cc
Kim Seonmin <w.hand715@gmail.com>
2024-05-20 20:54:24 +0900

    style : route name 에 따른 navbar 분기 처리 적용

--- frontend/movie-recommend/src/App.vue
+++ frontend/movie-recommend/src/App.vue
@@ -1,13 +1,15 @@
 <script setup>
-import { RouterLink, RouterView } from "vue-router";
+import { RouterLink, RouterView, useRoute} from "vue-router";
 import MovieNavbar from "@/components/common/MovieNavbar.vue";
 import OTTNavbar from "@/components/common/OTTNavbar.vue";
+
+const route = useRoute();
 </script>
 
 <template>
   <!-- route 에 따라 Navbar 변경되는 분기 처리 필요 -->
-  <!-- <MovieNavbar /> -->
-  <OTTNavbar />
+  <MovieNavbar v-if="route.name === 'Home' || route.name === 'MovieDetailView' || route.name === 'AIRecommendView'"/>
+  <OTTNavbar v-else/>
   <img class="background-img" 
   src="@/assets/main-img.png" alt="">
   <div class="background">
@@ -17,6 +19,7 @@ import OTTNavbar from "@/components/common/OTTNavbar.vue";
     <RouterView/>
   </div>
 </template>
+
 
 <style scoped>
 .background {
